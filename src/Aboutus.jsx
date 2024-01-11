f35f6cd98c6cd24791b54e3bf8d759557b693872
AkritiSubedi <aakusubedi075@gmail.com>
2024-01-11 12:37:46 +0545

    jina1

---
+++ src/Aboutus.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+
+function Aboutus() {
+  return (
+
+      <div className="maincontainer">
+        <div className="sub">
+          <img src="../public/jinalch 1.png" alt="" />
+        </div>
+        <div className="secondcontainer">
+          <div className="text">
+            <h1>Our Story</h1>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            consequat diam non lorem rhoncus ultrices. Nunc id quam ut arcu
+            bibendum faucibus at at arcu. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nunc consequat diam non lorem rhoncus
+            ultrices. Nunc id quam ut arcu bibendum faucibus at at arcu. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat
+            diam non lorem rhoncus ultrices. Nunc id quam ut arcu bibendum
+            faucibus at at arcu. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Nunc consequat diam non lorem rhoncus ultrices.
+            Nunc id quam ut arcu bibendum faucibus at at arcu.
+          </p>
+          <div className="thirdcontainer">
+            <img className="img" src="../public/Group.svg" alt="" />
+        </div>
+        </div>
+        
+      </div>
+
+  );
+}
+export default Aboutus;
